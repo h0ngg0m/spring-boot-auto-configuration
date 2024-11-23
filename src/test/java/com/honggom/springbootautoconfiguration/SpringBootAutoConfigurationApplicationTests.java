633f6233d16135a181833c6c5189907c5a47ccb1
h0ngg0m <jaehongbyeon98@gmail.com>
2024-11-23 17:08:58 +0900

    :sparkles: auto config된 빈 정보 출력

---
+++ src/test/java/com/honggom/springbootautoconfiguration/SpringBootAutoConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honggom.springbootautoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
